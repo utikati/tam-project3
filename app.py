8e70490a76a499de15c03ae2e090040f2c2e87fb
JMartins <60708319+utikati@users.noreply.github.com>
2022-06-07 12:07:59 +0100

    Registo e Login

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 ##
-## ===========================================
-## =============== API TESTE ===============
-## ===========================================
-## =======2020/2021 ==============================
-## ===========================================
-## ===========================================
+# ===========================================
+# =============== API TAM-Project3 ===============
+# ===========================================
+# =======2021/22 ==============================
+# ===========================================
+# ===========================================
 ##
-## Authors: 
-##   Goncalo Marques 
+# Authors:
+# Jorge Martins
 
 from flask import Flask, jsonify, request
-import logging, time, psycopg2, jwt, json
+import logging
+import time
+import psycopg2
+import jwt
+import json
 from datetime import datetime, timedelta
 from functools import wraps
 import os
-  
-app = Flask(__name__)   
+
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'it\xb5u\xc3\xaf\xc1Q\xb9\n\x92W\tB\xe4\xfe__\x87\x8c}\xe9\x1e\xb8\x0f'
 
@@ -27,14 +31,15 @@ UNAUTHORIZED_CODE = 401
 FORBIDDEN_CODE = 403
 NOT_FOUND = 404
 SERVER_ERROR = 500
-  
-@app.route('/', methods = ["GET"])
+
+
+@app.route('/', methods=["GET"])
 def home():
     return "Bem vindo à API!"
 
 
 ##########################################################
-## TOKEN INTERCEPTOR
+# TOKEN INTERCEPTOR
 ##########################################################
 def auth_user(func):
     @wraps(func)
@@ -45,9 +50,10 @@ def auth_user(func):
 
         try:
             token = content["token"]
-            data = jwt.decode(token, app.config['SECRET_KEY'])    
+            data = jwt.decode(token, app.config['SECRET_KEY'])
 
-            decoded_token = jwt.decode(content['token'], app.config['SECRET_KEY'])
+            decoded_token = jwt.decode(
+                content['token'], app.config['SECRET_KEY'])
             if(decoded_token["expiration"] < str(datetime.utcnow())):
                 return jsonify({"Erro": "O Token expirou!", "Code": NOT_FOUND_CODE})
 
@@ -58,22 +64,22 @@ def auth_user(func):
 
 
 ##########################################################
-## LOGIN
+# LOGIN
 ##########################################################
 @app.route("/login", methods=['POST'])
 def login():
     content = request.get_json()
 
-    if "n_identificacao" not in content or "senha" not in content:
+    if "username" not in content or "password" not in content:
         return jsonify({"Code": BAD_REQUEST_CODE, "Erro": "Parâmetros inválidos"})
 
     get_user_info = """
                 SELECT *
                 FROM utilizadores
-                WHERE n_identificacao = %s AND senha = crypt(%s, senha);
+                WHERE user_name = %s AND user_password = crypt(%s, senha);
                 """
 
-    values = [content["n_identificacao"], content["senha"]]
+    values = [content["username"], content["password"]]
 
     try:
         with db_connection() as conn:
@@ -90,37 +96,67 @@ def login():
         print(error)
         return jsonify({"Code": NOT_FOUND_CODE, "Erro": "Utilizador não encontrado"})
     return {"Code": OK_CODE, 'Token': token.decode('utf-8')}
-  
 
 ##########################################################
-## REGISTO DE UTILIZADOR
+# Verificar Existência de Utilizador
 ##########################################################
-@app.route("/registar_utilizador", methods=['POST'])
-def registar_utilizador():
+
+
+def verificar_username(username) -> bool:
     content = request.get_json()
 
-    if "n_identificacao" not in content or "nome" not in content or "senha" not in content or "email" not in content or "cargo" not in content: 
-        return jsonify({"Code": BAD_REQUEST_CODE, "Erro": "Parâmetros inválidos"})
-
     get_user_info = """
-                INSERT INTO utilizadores(n_identificacao, nome, senha, email, cargo, administrador) 
-                VALUES(%s, %s, crypt(%s, gen_salt('bf')), %s, %s, FALSE);
+                SELECT *
+                FROM utilizadores
+                WHERE user_name = %s;
                 """
 
-    values = [content["n_identificacao"], content["nome"], content["senha"], content["email"], content["cargo"]]
+    values = [content[username]]
 
     try:
         with db_connection() as conn:
             with conn.cursor() as cursor:
                 cursor.execute(get_user_info, values)
+                rows = cursor.fetchall()
         conn.close()
     except (Exception, psycopg2.DatabaseError) as error:
-        return jsonify({"Code": NOT_FOUND_CODE, "Erro": str(error)})
-    return {"Code": OK_CODE}
+        print(error)
+        return False
+    return True
+
+##########################################################
+# REGISTO DE UTILIZADOR
+##########################################################
+
+
+@app.route("/registar_utilizador", methods=['POST'])
+def registar_utilizador():
+    content = request.get_json()
+
+    if "username" not in content or "password" not in content:
+        return jsonify({"Code": BAD_REQUEST_CODE, "Erro": "Parâmetros inválidos"})
+
+    get_user_info = """
+                INSERT INTO useron(user_name, user_password) 
+                VALUES(%s, crypt(%s, gen_salt('bf')));
+                """
+
+    values = [content["username"], content["password"]]
+    if verificar_username(content["username"]):
+        return jsonify({"Code": BAD_REQUEST_CODE, "Erro": "Utilizador já existe"})
+    else:
+        try:
+            with db_connection() as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute(get_user_info, values)
+            conn.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            return jsonify({"Code": NOT_FOUND_CODE, "Erro": str(error)})
+        return {"Code": OK_CODE}
 
 
 ##########################################################
-## CONSULTAR SALDO
+# CONSULTAR LISTAS
 ##########################################################
 @app.route("/consultar_saldo", methods=['POST'])
 @auth_user
@@ -133,15 +169,15 @@ def consultar_saldo():
 
     decoded_token = jwt.decode(content['token'], app.config['SECRET_KEY'])
 
-    cur.execute("SELECT CAST(CAST(saldo AS NUMERIC(8,2)) AS VARCHAR) FROM utilizadores WHERE id = %s;", (decoded_token["id"],))
+    cur.execute("SELECT CAST(CAST(saldo AS NUMERIC(8,2)) AS VARCHAR) FROM utilizadores WHERE id = %s;",
+                (decoded_token["id"],))
     rows = cur.fetchall()
     conn.close()
     return {"Saldo": rows[0][0]}
 
 
-
 ##########################################################
-## CONSULTAR UTILIZADOR
+# CONSULTAR TAREFAS
 ##########################################################
 @app.route("/consultar_utilizador", methods=['POST'])
 @auth_user
@@ -153,44 +189,24 @@ def consultar_utilizador():
 
     decoded_token = jwt.decode(content['token'], app.config['SECRET_KEY'])
 
-    cur.execute("SELECT * FROM utilizadores WHERE id = %s;", (decoded_token["id"],))
+    cur.execute("SELECT * FROM utilizadores WHERE id = %s;",
+                (decoded_token["id"],))
     rows = cur.fetchall()
 
     conn.close()
     return jsonify({"Id": rows[0][1], "nome": rows[0][2], "e-mail": rows[0][4], "cargo": rows[0][6]})
 
-
-
-
 ##########################################################
-## LISTAR SE E ADMIN
+# ACTUALIZAR UTILIZADOR
 ##########################################################
-@app.route("/isAdmin", methods=['POST'])
-@auth_user
-def isAdmin():
-
-    conn = db_connection()
-    cur = conn.cursor()
-    content = request.get_json()
-
-    decoded_token = jwt.decode(content['token'], app.config['SECRET_KEY'])
-
-    cur.execute("SELECT administrador FROM utilizadores WHERE id = %s;", (decoded_token["id"],))
-    rows = cur.fetchall()
-    conn.close()
-    return {"admin": rows[0][0]}
 
 
-
-##########################################################
-## ACTUALIZAR UTILIZADOR
-##########################################################
 @app.route("/actualizar_utilizador", methods=['POST'])
 @auth_user
 def actualizar_utilizador():
     content = request.get_json()
 
-    if "nome" not in content or "email" not in content: 
+    if "nome" not in content or "email" not in content:
         return jsonify({"Code": BAD_REQUEST_CODE, "Erro": "Parâmetros inválidos"})
 
     get_user_info = """
@@ -210,7 +226,7 @@ def actualizar_utilizador():
 
 
 ##########################################################
-## CARREGAR SALDO
+# CARREGAR SALDO
 ##########################################################
 @app.route("/carregar_saldo", methods=['POST'])
 @auth_user
@@ -240,45 +256,12 @@ def carregar_saldo():
     return {"Code": OK_CODE}
 
 ##########################################################
-## ENVIAR REPORTS
+# DATABASE ACCESS
 ##########################################################
-@app.route("/enviar_report", methods=['POST'])
-@auth_user
-def enviar_report():
-    content = request.get_json()
-
-    if "assunto" not in content or "mensagem" not in content or "info" not in content or "anonimo" not in content:
-        return jsonify({"Code": BAD_REQUEST_CODE, "Erro": "Parâmetros inválidos"})
-
-    insert_report = """
-                INSERT INTO report(assunto, mensagem, utilizador_id, info_dispositivo, data_envio) VALUES(%s, %s, %s, %s, now());
-                """
-
-    decoded_token = jwt.decode(content['token'], app.config['SECRET_KEY'])
-    
-    if content["anonimo"] == "True":
-        user = None
-    else:
-        user = decoded_token["id"]
-
-    values = [content["assunto"], content["mensagem"], user, content["info"]]
-
-    try:
-        with db_connection() as conn:
-            with conn.cursor() as cursor:
-                cursor.execute(insert_report, values)
-        conn.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        return jsonify({"Code": NOT_FOUND_CODE, "Erro": "Report não registado"})
-    return {"Code": OK_CODE}
 
 
-
-##########################################################
-## DATABASE ACCESS
-##########################################################
 def db_connection():
-    
+
     DATABASE_URL = os.environ.get('DATABASE_URL')
     db = psycopg2.connect(DATABASE_URL)
     return db
